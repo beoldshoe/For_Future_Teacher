66e1cd07877741980d6be091da52ae7d5e267617
김영인 <young-in@gim-yeong-in-ui-MacBookPro.local>
2024-05-30 19:29:40 +0900

    [feat] completed basic setting

---
+++ future-teacher/src/main/java/com/server/future_teacher/FutureTeacherApplication.java
@@ -0,0 +1,13 @@
+package com.server.future_teacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureTeacherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutureTeacherApplication.class, args);
+	}
+
+}
---
+++ future-teacher/src/test/java/com/server/future_teacher/FutureTeacherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.future_teacher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutureTeacherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
